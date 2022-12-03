366ccfba8943631827044ff00434fd6c6b247f91
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-12-03 13:43:39 +0100

    chore: improve laravel mix config and add svelte confetti (WIP)

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,14 +2,30 @@
 
 let mix = require('laravel-mix');
 
-require('laravel-mix-svelte');
+// https://laravel-mix.com/extensions/svelte
+require("laravel-mix-svelte");
 
-mix.js('src/app.js', 'dist').setPublicPath('dist');
+// leave empty if you need to handle multiple package dirs
+mix.setPublicPath('');
 
+// compilation of js, 'input folder','output folder'
+mix.js('packages/svelte-demo/Resources/Private/JavaScript/main.js', 'packages/svelte-demo/Resources/Public/CompiledJavaScript/main.js');
 
+// add svelte support
+mix.svelte({
+    dev: !mix.inProduction()
+});
 
-mix.js('resources/js/app.js', 'public/js')
-    // .sass('resources/sass/app.scss', 'public/css')
-    .svelte({
-        dev: !mix.inProduction()
-    });
+// TODO: add sass support?
+/*
+mix.sass('src/app.scss','dist').options({
+    // https://laravel.com/docs/8.x/mix#url-processing
+    processCssUrls: false
+});*/
+
+// https://github.com/drud/ddev-browsersync#laravel-mix-configuration
+mix.browserSync({
+    proxy: "localhost",
+    open: false,
+    ui: false
+});
